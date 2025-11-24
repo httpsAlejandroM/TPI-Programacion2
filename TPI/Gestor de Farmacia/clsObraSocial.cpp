7366d67faaa9d0f06d3e1c412dce81b8b8c018d7
Esteban Vladimir Reyes <estebanvladimirreyes@gmail.com>
2025-11-24 14:53:41 -0300

    entidad ObraSocial .cpp y .h

---
+++ TPI/Gestor de Farmacia/clsObraSocial.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+#include "clsObraSocial.h"
+
+ObraSocial::ObraSocial() {
+    _idOS = 0;
+    _nombre[0] = '\0';
+    _porcentajeDescuento = 0.0;
+    _estado = false;
+}
+
+ObraSocial::ObraSocial(int idOS, const char* nombre, float porcentajeDescuento, bool estado) {
+    _idOS = idOS;
+    strcpy(_nombre, nombre);
+    _porcentajeDescuento = porcentajeDescuento;
+    _estado = estado;
+}
+
+void ObraSocial::setIdOS(int idOS) {
+    _idOS = idOS;
+}
+
+int ObraSocial::getIdOS() {
+    return _idOS;
+}
+
+void ObraSocial::setNombre(const char* nombre) {
+    strcpy(_nombre, nombre);
+}
+
+const char* ObraSocial::getNombre() {
+    return _nombre;
+}
+
+void ObraSocial::setPorcentajeDescuento(float porcentajeDescuento) {
+    _porcentajeDescuento = porcentajeDescuento;
+}
+
+float ObraSocial::getPorcentajeDescuento() {
+    return _porcentajeDescuento;
+}
+
+void ObraSocial::setEstado(bool estado) {
+    _estado = estado;
+}
+
+bool ObraSocial::getEstado() {
+    return _estado;
+}
